207b691e42a27cd3325ae0dffd67757861c3c177
Prajwol <lamichhaneprajwool@gmail.com>
2026-02-28 18:56:52 +0545

    Implemented streaming in Chatbot

---
+++ Chatbot/langraph_backend.py
@@ -0,0 +1,38 @@
+from langgraph.graph import StateGraph,START,END
+from typing import TypedDict,Annotated
+from langchain_core.messages import BaseMessage,HumanMessage
+from langchain_openai import ChatOpenAI
+from langgraph.checkpoint.memory import MemorySaver
+
+from langgraph.graph.message import add_messages
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class ChatState(TypedDict):
+    messages:Annotated[list[BaseMessage],add_messages]
+
+
+llm=ChatOpenAI()
+def chat_node(state:ChatState):
+    messages=state['messages']
+
+    response=llm.invoke(messages)
+
+    return{'messages':[response]}
+
+
+checkpointer=MemorySaver()
+graph=StateGraph(ChatState)
+
+graph.add_node('chat_node',chat_node)
+graph.add_edge(START,'chat_node')
+graph.add_edge('chat_node',END)
+
+chatbot=graph.compile(checkpointer=checkpointer)
+
+
+chatbot=graph.compile(checkpointer=checkpointer)
